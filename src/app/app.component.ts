3622400f6463a0b24209c76733f8efb1eff25d60
Carlos Gutierrez <feucarlos@gmail.com>
2020-10-29 19:35:44 -0600

    menu actualizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,33 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core"
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Platform } from "@ionic/angular"
+import { SplashScreen } from "@ionic-native/splash-screen/ngx"
+import { StatusBar } from "@ionic-native/status-bar/ngx"
+import { DataService } from "./services/data.service"
+import { Observable } from "rxjs"
+import { Componente } from "./interfaces/interfaces"
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  selector: "app-root",
+  templateUrl: "app.component.html",
+  styleUrls: ["app.component.scss"],
 })
 export class AppComponent {
+  componentes: Observable<Componente[]>
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private dataService: DataService
   ) {
-    this.initializeApp();
+    this.initializeApp()
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
+      this.statusBar.styleDefault()
+      this.splashScreen.hide()
+      this.componentes = this.dataService.getMenuOpt()
+    })
   }
 }
